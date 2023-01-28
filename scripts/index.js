ccc02eb9394c92d9671a33105478b6e47164ec79
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-01-28 18:16:07 +0300

    refactor hover on case

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+const porfolioItemList = document.querySelectorAll('.portfolio__item');
 
 const handleHover = (evt) => {
   const contentElement = evt.target.querySelector('.portfolio__item-content');
@@ -5,10 +6,18 @@ const handleHover = (evt) => {
   if (contentElement 
     && contentElement.classList 
     && !contentElement.classList.contains('portfolio__item-content_visible')) {
+      
+      porfolioItemList.forEach((el) => {
+        const contElPrevious = el.querySelector('.portfolio__item-content');
+        if (contElPrevious && contElPrevious.classList) {
+          contElPrevious.classList.remove('portfolio__item-content_visible');
+        }
+      });
+
       contentElement.classList.add('portfolio__item-content_visible');
     }  
 }
 
-document.querySelectorAll('.portfolio__item').forEach( (element) => {
+porfolioItemList.forEach( (element) => {
   element.addEventListener('mouseover', handleHover);
 })
