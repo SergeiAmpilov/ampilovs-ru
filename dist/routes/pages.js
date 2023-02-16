12ffdbec4ea3c02e0f08b46ff04adfeddeae663f
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-16 21:44:24 +0300

    refactor

--- dist/routes/pages.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pagesRouter = void 0;
-const express_1 = require("express");
-const pagesRouter = (0, express_1.Router)();
-exports.pagesRouter = pagesRouter;
-pagesRouter.get('/', (req, res, next) => {
-    res.render('index');
-});
