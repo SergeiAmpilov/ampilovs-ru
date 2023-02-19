611298e8083d4662727b90d7100dc05421b4f729
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-19 21:20:00 +0300

    feat: page 404

---
+++ public_html/404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="style/index.css">
+    <title>404 - страница не найдена | Программист Ампилов</title>
+  </head>
+  <body class="page-404">
+    <div class="container">
+      <div class="page-404__content">
+        <img class="page-404__img" src="./images/oops.png" alt="oops" >
+        <h1 class="page-404__title">404 - page not found</h1>      
+        <p class="page-404__text">К сожалению, запрашиваемая страница не найдена</p>
+        <a class="promo__link promo__link_color-blue" href="/">Вернуться на главную</a>
+      </div>
+    </div>
+  </body>
+</html>
+.promo__link_color-blue
